d82bb22a9dc83498b873391b23135eaa0225b49b
Бажен Левковець <bazhen.paseka@gmail.com>
2019-09-25 21:56:14 +0300

    File APPEND
    1

--- Middlewares/Third_Party/FatFs/src/ff.h
+++ Middlewares/Third_Party/FatFs/src/ff.h
@@ -314,7 +314,6 @@ int ff_del_syncobj (_SYNC_t sobj);				/* Delete a sync object */
 #define	FA_CREATE_ALWAYS	0x08
 #define	FA_OPEN_ALWAYS		0x10
 #define FA__WRITTEN			0x20
-#define FA_OPEN_APPEND		0x30
 #define FA__DIRTY			0x40
 #endif
 
